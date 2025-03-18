6b593501f8612939030b63b4cf51ff3e62ab85a1
TTaiJin <taeparis980110@gmail.com>
2025-03-18 12:30:40 +0900

    work

---
+++ src/main/kotlin/com/ll/backend/FiyCiCd1Application.kt
@@ -0,0 +1,11 @@
+package com.ll.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FiyCiCd1Application
+
+fun main(args: Array<String>) {
+    runApplication<FiyCiCd1Application>(*args)
+}
